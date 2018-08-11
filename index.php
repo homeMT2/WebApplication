b910ca7ff35242cfc1727359df3163aefc8eff9c
Ivan <homeMT2@gmail.com>
2018-08-11 14:36:06 +0300

    New Include

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 include_once 'web.php';
+include_once 'web-pg.php';
+
+//phpinfo();
 
 function show( $array = array() ) {
 
@@ -15,3 +18,11 @@ $test = new \WebApplication1\WebService1();
 $result = $test->getdatafromextra_adv_t( 'cino1' );
 
 show( $result );
+
+//
+//CREATE TABLE extra_adv_t (did integer, cino varchar(40) );
+//
+//INSERT INTO weather (did, cino) VALUES (123, 'cino1');
+//INSERT INTO weather (did, cino) VALUES (444, 'cino1');
+//INSERT INTO weather (did, cino) VALUES (444, 'cino');
+//INSERT INTO weather (did, cino) VALUES (444, 'cino5');
